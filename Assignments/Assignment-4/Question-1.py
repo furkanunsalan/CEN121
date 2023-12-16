35cd08f7f006bd89a87a5702a12d94eebf668814
Furkan Ünsalan <me@furkanunsalan.dev>
2023-12-16 22:19:37 +0300

    assignment-4

---
+++ Assignments/Assignment-4/Question-1.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+def solution():
+    number_of_peple = [10, 45, 20, 60, 30]
+    ages = ["10-20", "20-30", "30-40", "50-60", "60+"]
+
+    plt.bar(ages, number_of_peple, color= "blue")
+    plt.title("People Ages")
+
+    plt.xlabel("Ages")
+    plt.ylabel("Number of People")
+
+    plt.show()
+
+solution()

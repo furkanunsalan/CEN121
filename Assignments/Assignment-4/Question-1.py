a9092111d8490cfe19596cb9b9b2edead436cb65
Furkan Ünsalan <furkanunsalan@gmail.com>
2023-12-18 01:11:07 +0300

    ufak bir fix

--- Assignments/Assignment-4/Question-1.py
+++ Assignments/Assignment-4/Question-1.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 def solution():
-    number_of_peple = [10, 45, 20, 60, 30]
+    number_of_peple = [10, 45, 20, 60, 35]
     ages = ["10-20", "20-30", "30-40", "50-60", "60+"]
 
     plt.bar(ages, number_of_peple, color= "blue")

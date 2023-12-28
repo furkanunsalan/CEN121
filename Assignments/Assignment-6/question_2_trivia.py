5ecd043a470b3deb9ac52f3320caced23c89601a
Furkan Ünsalan <me@furkanunsalan.dev>
2023-12-28 22:32:40 +0300

    assignment-6 question2

--- Assignments/Assignment-6/question_2_trivia.py
+++ Assignments/Assignment-6/question_2_trivia.py
@@ -1,62 +1,53 @@
-from question_2_player import Player
-
-question = {
-        "Q1": ["a", ["a", "b", "c", "d"]],
-        "Q2": ["b", ["a", "b", "c", "d"]],
-        "Q3": ["c", ["a", "b", "c", "d"]],
-        "Q4": ["d", ["a", "b", "c", "d"]],
-        #"Q5": ["a", ["a", "b", "c", "d"]],
-        #"Q6": ["b", ["a", "b", "c", "d"]],
-        #"Q7": ["c", ["a", "b", "c", "d"]],
-        #"Q8": ["d", ["a", "b", "c", "d"]],
-        #"Q9": ["a", ["a", "b", "c", "d"]],
-        #"Q10": ["b", ["a", "b", "c", "d"]],
-        }
-
-player1 = Player("Player 1", 1)
-player2 = Player("Player 2", 2)
+from question_2_player import player1, player2
+from question_2_questions import questions
 
 
 class Trivia:
-    def __init__(self, player):
-        self.player = player
+    def __init__(self, player1, player2, current_player=player1):
+        self.player1 = player1
+        self.player2 = player2
+        self.current_player = current_player
+    
+    def change_turn(self):
+        if self.current_player == player1:
+            self.current_player = player2
+        else:
+            self.current_player = player1
         
     
     def ask_questions(self):
-        for key in question:
-            print(f"{self.player.name}'s turn")
+        for key in questions:
+            print()
+            print(f"{self.current_player.name}'s turn")
             print(key)
+            print()
             
-            for choice in question[key][1]:
+            for choice in questions[key][1]:
                 print(choice)
+            print()
                 
-            answer = input("Enter your answer: ")
+            answer = input("Enter the answer phrase: ")
             
-            if answer.lower() == question[key][0]:
-                print("Correct!")
-                if self.player.player_no == 1:
-                    player1.change_score(1)
-                    self.player = player2
-                else:
-                    player2.change_score(1)
-                    self.player = player1
+            if answer.lower() == questions[key][0]:
+                print("Correct Answer!")
+                self.current_player.change_score(1)
+                self.change_turn()
                     
             else:
-                print("Incorrect!")
-                if self.player.player_no == 1:
-                    self.player = player2
-                else:
-                    self.player = player1
+                print(f"Incorrect Answer! The correct answer is {questions[key][0].upper()}")
+                self.change_turn()
             
     def evaluate_winner(self):
-        print(f"{player1.name}'s score: ")
-        print(f"{player2.name}'s score: ")
+        print()
+        print(f"{player1.name}'s score: {player1.score}")
+        print(f"{player2.name}'s score: {player2.score}")
         
         if player1.score > player2.score:
-            return f"{player1.name} wins!"
+            return f"{player1.name} wins the game!"
         
         elif player1.score < player2.score:
-            return f"{player2.name} wins!"
+            return f"{player2.name} wins the game!"
         else:
-            return "Tie!"
+            return f"{player1.name} and {player2.name} are tied!"
+        
             
